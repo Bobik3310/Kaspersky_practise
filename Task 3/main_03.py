7eb3e332f67284ad9ddfd0110dd6c23467040023
123qwertyuiopas <pimenoveu12@gmail.com>
2022-06-02 13:58:36 +0300

    Discription

---
+++ Task 3/main_03.py
@@ -0,0 +1,7 @@
+alph = 'qwertyuiopasdfghjklzxcvbnm_{}1234567890'
+text = list('yourflagis{fhke37_kdrjknbmpr_04374j}')
+gamma = 'thekey'
+for count, i in enumerate(text, start=0):
+    text[count] = alph[(alph.index(i) + alph.index(gamma[count % len(gamma)])) % len(alph)]
+print(*text, sep='')
+# Спроси у Четверга
