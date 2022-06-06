0711356cc6ee0f43397646c91c0a98c32826441b
123qwertyuiopas <pimenoveu12@gmail.com>
2022-06-06 21:59:01 +0300

    Discription

--- Task 2/main_02_input.py
+++
@@ -1,5 +0,0 @@
-shifr = list(map(int, input().split('|')))
-alph = [chr(i) for i in range(ord('a'), ord('z') + 1)]
-for count, i in enumerate(shifr, start=0):
-    shifr[count] = alph[i - 1]
-print(''.join(shifr))
